090e11adc6fb0ceb6b2726cd2f8143723221d47f
SEU-AI-LYFDNFF <60429420+BrilliantGIRL@users.noreply.github.com>
2021-02-28 23:00:15 +0800

    网页外部框架
    地图部分空缺，留出按钮函数接口，topnav链接待补充，样式待调整

---
+++ myScript.js
@@ -0,0 +1,20 @@
+var sidebtn = new Vue({
+    el:'#selector',
+    methods:{
+        button1:function(){
+            //wait to put things here
+            alert("1")
+        },
+
+        button2:function(){
+            //
+            alert("2")
+        },
+
+        button3:function(){
+            //
+            alert("3")
+        },
+    }
+
+})
